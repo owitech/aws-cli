db86b7a1fc94964429a4765c19e7898d28c8f464
Julio Castillo <121035941+owitech@users.noreply.github.com>
2023-03-02 15:02:49 -0500

    use create-tags

--- vpc-test.sh
+++ vpc-test.sh
@@ -1,44 +1,73 @@
 #!/bin/bash
 
+# Variables
 REGION="us-east-1"
+VPC_NAME="vpc-test"
 VPC_CIDR="10.0.0.0/25"
-MY_IP="$(curl -s checkip.amazonaws.com 2>&1)/32"
 
 SUBNET_AZ="us-east-1a"
-SUBNET_PUBLIC="10.0.0.0/26"
-SUBNET_PRIVATE="10.0.0.64/26"
+MY_IP="$(curl -s checkip.amazonaws.com 2>&1)/32"
 
-SG_NAME="WebPublicBasicGroup"
+SUBNET_PUBLIC_CIDR="10.0.0.0/26"
+SUBNET_PUBLIC_NAME="subnet-test-public"
+
+SUBNET_PRIVATE_CIDR="10.0.0.64/26"
+SUBNET_PRIVATE_NAME="subnet-test-private"
+
+GROUP_NAME="WebPublicBasicGroup"
+GROUP_DESC="SG for Subnet Public"
+
+
 
 # Create Vpc
+echo "Creando la VPC..."
 VPC_ID=$(aws ec2 create-vpc \
     --cidr-block $VPC_CIDR \
     --region $REGION \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=vpc-basic-01}]' \
     --query "Vpc.VpcId" \
     --output text)
+
+# Agregar etiquetas a la VPC
+echo "Agregando etiquetas a la VPC..."
+aws ec2 create-tags --resources $VPC_ID --tags Key=Name,Value=$VPC_NAME
+
+# Activar la resolución de DNS para la VPC
+echo "Activando la resolución de DNS para la VPC..."
+aws ec2 modify-vpc-attribute --vpc-id $VPC_ID --enable-dns-support "{\"Value\":true}"
+aws ec2 modify-vpc-attribute --vpc-id $VPC_ID --enable-dns-hostnames "{\"Value\":true}"
+
+
 
 # Create Subnet Public
 SUBNET_PUBLIC_ID=$(aws ec2 create-subnet \
     --availability-zone $SUBNET_AZ \
-    --cidr-block $SUBNET_PUBLIC \
+    --cidr-block $SUBNET_PUBLIC_CIDR \
     --vpc-id $VPC_ID \
-    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=subnet-basic-public-01}]' \
     --query "Subnet.SubnetId" \
     --output text)
+
+# Agregar etiquetas a la subred pública
+echo "Agregando etiquetas a la subred pública..."
+aws ec2 create-tags --resources $SUBNET_PUBLIC_ID --tags Key=Name,Value=$SUBNET_PUBLIC_NAME
+
+
 
 # Create Subnet Private
 SUBNET_PRIVATE_ID=$(aws ec2 create-subnet \
     --availability-zone $SUBNET_AZ \
-    --cidr-block $SUBNET_PRIVATE \
+    --cidr-block $SUBNET_PRIVATE_CIDR \
     --vpc-id $VPC_ID \
-    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=subnet-basic-private-01}]' \
     --query "Subnet.SubnetId" \
     --output text)
 
+# Agregar etiquetas a la subred privada
+echo "Agregando etiquetas a la subred privada..."
+aws ec2 create-tags --resources $SUBNET_PRIVATE_ID --tags Key=Name,Value=$SUBNET_PRIVATE_NAME
+
+
+
 # Create Interne Gateway
 IGW_ID=$(aws ec2 create-internet-gateway \
-    --tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Name,Value=igw-basic-01}]' \
     --query "InternetGateway.InternetGatewayId" \
     --output text)
 
@@ -48,35 +77,39 @@ aws ec2 attach-internet-gateway \
     --vpc-id $VPC_ID
 
 # Create Route-Table and associate with Vpc
-RTB_ID=$(aws ec2 create-route-table \
+# --filters Name=vpc-id,Values=$VPC_ID \ ???
+ROUTE_TABLE_ID=$(aws ec2 create-route-table \
     --vpc-id $VPC_ID \
-    --tag-specifications 'ResourceType=route-table,Tags=[{Key=Name,Value=rt-basic-01}]' \
     --query "RouteTable.RouteTableId" \
     --output text)
 # Por defector crea un route 10.0.0.0/25 -> local
 
 # Create and associate a Route with Route-Table and Internet Gateway
 aws ec2 create-route \
-    --route-table-id $RTB_ID \
+    --route-table-id $ROUTE_TABLE_ID \
     --destination-cidr-block "0.0.0.0/0" \
     --gateway-id $IGW_ID
 
 # Associate Route-Table-Rule with Subnet-Public
 RTB_ASSOC_PUBLIC_ID=$(aws ec2 associate-route-table \
-    --route-table-id $RTB_ID \
+    --route-table-id $ROUTE_TABLE_ID \
     --subnet-id $SUBNET_PUBLIC_ID \
     --query "AssociationId" \
     --output text)
 
+
+
 # Create Security Group
+echo "Creando un grupo de seguridad para la instancia..."
 GROUP_PUBLIC_ID=$(aws ec2 create-security-group \
-    --group-name $SG_NAME \
-    --description "SG for Subnet Public" \
+    --group-name $GROUP_NAME \
+    --description $GROUP_DESC \
     --vpc-id $VPC_ID \
-    --tag-specifications 'ResourceType=security-group,Tags=[{Key=Name,Value=sg-basic-public-01}]' \
     --query "GroupId" \
     --output text)
 
+#Agregar reglas de entrada y salida para el grupo de seguridad
+echo "Agregando reglas de entrada y salida para el grupo de seguridad..."
 # Create an Security-Group-Rule SSH
 SGR_PUBLIC_SSH=$(aws ec2 authorize-security-group-ingress \
     --group-id $GROUP_PUBLIC_ID \
@@ -104,7 +137,7 @@ echo "VPC ID:                   $VPC_ID"                >> ids.txt
 echo "SUBNET PUBLIC ID:         $SUBNET_PUBLIC_ID"      >> ids.txt
 echo "SUBNET PRIVAT ID:         $SUBNET_PRIVATE_ID"     >> ids.txt
 echo "INTERNET GATEWAY ID:      $IGW_ID"                >> ids.txt
-echo "RT ID:                    $RTB_ID"                >> ids.txt
+echo "RT ID:                    $ROUTE_TABLE_ID"        >> ids.txt
 echo "RT ASSOCIATE PUBLIC ID:   $RTB_ASSOC_PUBLIC_ID"   >> ids.txt
 echo "SG PUBLIC ID:             $GROUP_PUBLIC_ID"       >> ids.txt
 echo "SGR PUBLIC SSH:           $SGR_PUBLIC_SSH"        >> ids.txt
