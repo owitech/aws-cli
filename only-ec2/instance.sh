6d84911323b3b911261bca7d2c2439b538e5effd
Julio Castillo <121035941+owitech@users.noreply.github.com>
2023-03-10 11:26:25 -0500

    variables were added to the /only-ec2/instance.sh file

--- only-ec2/instance.sh
+++ only-ec2/instance.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+IAM_ID="SSMEC2connect"
+AMI_ID="ami-006dcf34c09e50022"
+
+GROUP_ID="sg-0229084012065926e"
+SUBNET_ID="subnet-0310d3c34c7f23b55"
+
+USER_DATA="file://configure.txt"
+EBS_DEVICE="file://mapping.json"
+
 INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id ami-006dcf34c09e50022 \
+    --image-id $AMI_ID \
     --instance-type t2.micro \
-    --subnet-id subnet-01fe45d0e71c4361f \
-    --security-group-ids sg-025fd5c53b5952dae \
+    --subnet-id $SUBNET_ID \
+    --security-group-ids $GROUP_ID \
     --associate-public-ip-address \
     --key-name web \
-    --iam-instance-profile Name=SSMEC2connect \
+    --iam-instance-profile Name=$IAM_ID \
     --instance-initiated-shutdown-behavior stop \
     --count 1 \
-    --user-data file://configure.txt \
-    --block-device-mappings file://mapping.json \
+    --user-data $USER_DATA \
+    --block-device-mappings $EBS_DEVICE \
     --query 'Instances[0].InstanceId' \
     --output text)
 
-aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=i-web
+aws ec2 create-tags --resources $INSTANCE_ID --tags Key=Name,Value=i-web Key=Hostname,Value=ec2-web
 
+echo "Creating instance..."
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
 # Output instance information
